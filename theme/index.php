838ea42576062e443abe2ecc7593c4a5ea211d03
Matt Wamboldt <mattwamboldt@hotmail.com>
2018-12-03 17:35:00 -0400

    Set up pagination of post archives

--- theme/index.php
+++ theme/index.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
     <div class='container'>
+        <?php the_posts_pagination(); ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php // get_template_part('content', get_post_format()); 
         // template parts are php files that are looked up in the theme, the second param adds a -whatever to the end
         // This function is likely how wordpress handles the template hierarchy or at least some of the same base code ?>
-        <article>
+        <article class="blog-entry">
             <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
             <?php //the_post_thumbnail(); ?>
             <small>Posted <?php the_date(); ?> in: <?php the_category(', '); ?></small>
             <?php the_excerpt(); ?>
         </article>
     <?php endwhile; ?>
-    <div class="nav-previous alignleft"><?php previous_posts_link( 'Newer posts' ); ?></div>
-    <div class="nav-next alignright"><?php next_posts_link( 'Older posts' ); ?></div>
+    <?php the_posts_pagination(); ?>
     <?php else : ?>
         <h2>Nothing to see here, move along folks.</h2>
     <?php endif; ?>
